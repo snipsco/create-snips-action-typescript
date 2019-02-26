56dba4866dd1924295b01d6f6bb51847e418ef7d
elbywan <elbywan@hotmail.com>
2019-02-26 19:05:44 +0100

    add pokemon slot type

--- static/src/handlers/pokemon.ts
+++ static/src/handlers/pokemon.ts
@@ -2,12 +2,13 @@ import { getPokemon } from '../api'
 import { i18nFactory } from '../factories'
 import { message } from '../utils'
 import { Handler } from './index'
+import { NluSlot, slotType } from 'hermes-javascript'
 
 export const pokemonHandler: Handler = async function (msg, flow) {
     // Suppose we have a pokemon id slot
     // If there are multiple, we take the only that is supposed to be the 'most valid'.
     // We discard slots with a confidence value too low.
-    const pokemonSlot = message.getSlotsByName(msg, 'pokemon_id', { onlyMostConfident: true, threshold: 0.5 })
+    const pokemonSlot: NluSlot<slotType.custom> = message.getSlotsByName(msg, 'pokemon_id', { onlyMostConfident: true, threshold: 0.5 })
 
     // We need this slot, so if the slot had a low confidence or was not mark as required,
     // we throw an error.
